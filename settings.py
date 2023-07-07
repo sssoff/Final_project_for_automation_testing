1818d2456cc054112e8897fb04c15bea96b90361
sssoff <123302845+sssoff@users.noreply.github.com>
2023-07-07 17:23:04 +0300

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,26 @@
+class Settings:
+    chrome_driver = "/tests/chromedriver.exe"
+
+    base_url = 'https://b2c.passport.rt.ru'
+    valid_email = 'test.19fff@mail.ru'       # Ввести валидный email
+    valid_password = 'Test19fff'   # Ввести валидный пароль
+    valid_email_reg = 'Ввести валидный email'   # Ввести валидный email
+    invalid_email = 'sss_dd_F.com'
+    invalid_password = 'gfhfy123'
+    empty_email = ''
+    empty_password = ''
+    menu_type_auth = ['Телефон', 'Почта', 'Логин', 'Лицевой счёт']
+    placeholder_name = ['Мобильный телефон', 'Электронная почта', 'Логин', 'Лицевой счёт']
+    first_name = 'Лев'
+    last_name = 'Львович'
+    first_name_ = '-'
+    last_name_ = '-'
+    russian_generate_string = 'я'
+    latin_generate_string = 'invalid'
+    chinese_chars = ' 青山河凤龙月星雨'
+    special_chars = '()_+=/{}!@#$%^&*'
+    numbers = 740
+    empty = ''
+    passw1 = '12345678'
+    passw2 = 'qwerty'
+    passw3 = 'y5VxUwNzJb8KtLpD'
